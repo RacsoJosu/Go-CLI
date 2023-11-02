64fc59f26649d2f1d903983b4f9576512bf44c0c
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2023-11-01 22:45:05 -0600

    Aactualizacion channel

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/RacsoJosu/Api-Rest/server"
 )
 
 func main() {
+	ctx := context.Background()
+	// crear un channel
+	serverDoneChan := make(chan os.Signal, 1)
+	signal.Notify(serverDoneChan, os.Interrupt, syscall.SIGTERM)
 
 	srv := server.New(":3000")
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	log.Println("Server started")
+	<- serverDoneChan
+	srv.Shutdown(ctx)
+
+	
+	log.Println("Server stopped")
+	
 
 }
